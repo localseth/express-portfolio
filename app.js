847ca42920e9ed5a37d85a4bd3aabc7dd294bcd2
Seth M <97685638+localseth@users.noreply.github.com>
2022-05-26 18:00:31 -0500

    delete bin folder, add data to pug templates, create error handlers

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // initialize app
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// Express middleware for accessing the req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // set routes
 const routes = require('./routes/routes');
@@ -12,7 +18,40 @@ app.use(routes);
 
 app.use('/static', express.static('public'));
 
-app.listen(3000, () => {
-    console.log('The application is running on localhost:3000');
+/* ERROR HANDLERS */
+// custom error
+app.get('/error', (req, res, next) => {
+    console.log('Custom error route called');
+    const err = new Error();
+    err.message = `Something went wrong! Please try again later.`
+    err.status = 500;
+    err.title = 'Server Error'
+    throw err;
+});
+/* 404 handler to catch undefined or non-existent route requests */ 
+app.use((req, res, next) => {
+    console.log('404 error handler called');
+    const err = new Error();
+    err.message = "Sorry! We couldn't find the page you were looking for."
+    err.status = 404;
+    err.title = 'Page Not Found'
+    throw err;
+});
+  
+  /* Global error handler */
+app.use((err, req, res, next) => {
+  
+    if (err) {
+        console.log('Global error handler called', err);
+    }
+    if (err.status === 404) {
+        res.status(404).render('error', { err });
+    } else {
+        err.message = err.message || 'A server error has occured';
+        res.status(err.status || 500).render('error', { err });
+    }
 });
 
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000');
+})
--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const { projects } = require('data.json');
+const { projects } = require('../data');
 
 router.get('/', (req, res, next) => {
+    console.log('home route called');
     res.locals.projects = projects;
-    res.render('index');
+    res.render('index', projects);
 });
 
 router.get('/about', (req, res, next) => {
     res.render('about');
 });
 
+router.get('/projects')
+
 router.get('/projects/:id', (req, res, next) => {
-    const { id } = req.params;
-    res.render('project', projects[id]);
+    console.log(`Project ${req.params.id} has been called`);
+    if (projects[req.params.id]) {
+        const { id } = req.params;
+        res.render('project', projects[id]);
+    } else {
+        const err = new Error();
+        err.message = 'There is no project for the requested ID'
+        err.status = 404;
+        err.title = 'Project not found';
+        next(err);
+    }
 });
 
 module.exports = router;
